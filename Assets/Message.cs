ffbd0730e2307adf8b2377fd01569995d53c1d91
Diego <alu0101408903@ull.edu.es>
2024-09-22 19:21:16 +0100

    first commit

---
+++ Assets/Message.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Script tarea 1.2");
+    }
+}
